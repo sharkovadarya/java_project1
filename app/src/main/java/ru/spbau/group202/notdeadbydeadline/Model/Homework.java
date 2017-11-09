80b994869cf6769e05ff457ba6ee5a6465091c80
Dmitry <qudeed@gmail.com>
2017-11-09 19:22:05 +0300

    Added fields for Homework.

--- app/src/main/java/ru/spbau/group202/notdeadbydeadline/Model/Homework.java
+++ app/src/main/java/ru/spbau/group202/notdeadbydeadline/Model/Homework.java
@@ -1,12 +1,24 @@
 package ru.spbau.group202.notdeadbydeadline.Model;
+
 public class Homework {
     private Deadline deadline;
-    private String subject;
+    private String subject, description, howToSend;
+    private boolean isRegular;
+    private int expectedScore, actualScore;
 
     public Homework(String year, String month, String day,
-                    String hour, String minute, String subject) {
+                    String hour, String minute, String subject,
+                    boolean isRegular, String description, String howToSend, int expectedScore) {
         deadline = new Deadline(year, month, day, hour, minute);
         this.subject = subject;
+        this.isRegular = isRegular;
+        this.description = description;
+        this.howToSend = howToSend;
+        this.expectedScore = expectedScore;
+    }
+
+    public void setActualScore(int score) {
+        actualScore = score;
     }
 
     public void checkDeadline() {
