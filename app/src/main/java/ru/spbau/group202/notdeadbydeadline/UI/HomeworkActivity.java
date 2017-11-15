541d1f2582187dd2d254d53ae2231bcab95bd783
sharkovadarya <sharkovadarya@gmail.com>
2017-11-15 15:28:56 +0300

    Reworked adding homework with static Contoller.

--- app/src/main/java/ru/spbau/group202/notdeadbydeadline/UI/HomeworkActivity.java
+++ app/src/main/java/ru/spbau/group202/notdeadbydeadline/UI/HomeworkActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -21,13 +23,46 @@ import ru.spbau.group202.notdeadbydeadline.R;
 
 public class HomeworkActivity extends AppCompatActivity {
 
+    // TODO that's not how it's supposed to be
+    ArrayList<String> subjects = new ArrayList<>();
 
-    ArrayList<String> outputValues = new ArrayList<>();
-    private void putStringToOutputValues(String outputValue) {
-        outputValues.add(outputValue);
+    private void debuggingThrashMethod() {
+        subjects.add("Java");
+        subjects.add("Algebra");
+        subjects.add("Algorithms");
+        subjects.add("Functional Programming");
+        subjects.add("Discrete Mathematics");
     }
 
-    public void outputHomeworks() {
+    public void addButtons() {
+
+        // TODO Literally not how it's supposed to be
+        debuggingThrashMethod();
+
+        LinearLayout linearLayout = findViewById(R.id.homeworkActivityLinearLayout);
+        // TODO do I need to? It's already set in XML
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+        for (int i = 0; i < subjects.size(); ++i) {
+            LinearLayout row = new LinearLayout(this);
+            row.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            Button button = new Button(this);
+            button.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            String subject = subjects.get(i);
+            button.setText(subject);
+            button.setId(i);
+            row.addView(button);
+
+            linearLayout.addView(row);
+        }
+
+
 
     }
 
@@ -51,6 +86,8 @@ public class HomeworkActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle("Homework");
         }
+
+        addButtons();
     }
 
 }
